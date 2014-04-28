925af8dd6fef81a4adf859cb263ed8bbde935f5f
Nishant Bhorodia <nishant.bhorodia@thetigroup.com>
2014-04-28 18:52:45 +0530

    global removeAliens -> object based removeAlien

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,10 @@ var aliens = [];
 var alienId = 0;
 var score = 0;
 var total = 0;
+var imageSrc = ['assets/images/alien2.png', 'assets/images/alien.png', 'assets/images/alien1.png', 'assets/images/alien3.png', 'assets/images/asteroid.gif'];
 
 function Alien(left, top, maxleft, maxtop) {
-    this.imageSrc = ['assets/images/alien2.png', 'assets/images/alien.png', 'assets/images/alien1.png', 'assets/images/alien3.png', 'assets/images/asteroid.gif'];
+
     this.maxleft = maxleft;
     this.maxtop = maxtop;
 
@@ -12,15 +13,15 @@ function Alien(left, top, maxleft, maxtop) {
     this.image.id = alienId;
     ++alienId;
 
-    this.image.setAttribute("onclick", "this.className = 'removeAlien'; updateScore();");
-    this.image.src = this.imageSrc[Math.floor(Math.random() * this.imageSrc.length)];
+    this.image.setAttribute("onclick", "aliens[getAlienKey(this.id)].removeAlien(); updateScore();");
+    this.image.src = imageSrc[Math.floor(Math.random() * imageSrc.length)];
 
     this.image.style.height = "100px";
     this.image.style.position = "absolute";
     this.image.style.top = top;
     this.image.style.left = left;
 
-    this.move = function() {
+    this.move = function () {
         var x = parseInt(this.image.style.left);
         var y = parseInt(this.image.style.top);
         if (y <= this.maxtop) {
@@ -28,10 +29,19 @@ function Alien(left, top, maxleft, maxtop) {
             this.image.style.top = y;
             return true;
         } else {
-            this.image.className = "removeAlien";
+            this.removeAlien();
             return false;
         }
     };
+
+    this.removeAlien = function () {
+        var alien = document.getElementById(this.image.id);
+        var key = getAlienKey(this.image.id);
+        aliens.splice(key, 1);
+        document.body.removeChild(alien);
+        return false;
+    };
+
     document.body.appendChild(this.image);
 }
 
@@ -44,27 +54,12 @@ function createAliens() {
 }
 
 function moveAliens() {
-    removeAliens();
     for (var key in aliens) {
         aliens[key].move();
     }
     return false;
 }
 
-function removeAliens() {
-    var cells = document.getElementsByClassName('removeAlien');
-    var len = cells.length;
-    if (len !== 0) {
-        for (var i = 0; i < len; i++) {
-            for (var key in aliens) {
-                if (aliens[key].image.id === cells[i].id)
-                    aliens.splice(key, 1);
-            }
-            document.body.removeChild(cells[i]);
-        }
-    }
-    return false;
-}
 
 function updateTotalAliens() {
     document.getElementById('total').innerHTML = ++total;
@@ -72,4 +67,12 @@ function updateTotalAliens() {
 
 function updateScore() {
     document.getElementById('score').innerHTML = ++score;
+}
+
+function getAlienKey(id){
+    for (var key in aliens) {
+        if (aliens[key].image.id == id) {
+            return key;
+        }
+    }
 }
